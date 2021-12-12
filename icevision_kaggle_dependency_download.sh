b6efdd3f78e8a919628ff0a3a68a42fdcb5f6d52
Aaron Brown <brownaa@users.noreply.github.com>
2021-12-12 15:51:48 -0600

    fixed typos and update progress heading

--- icevision_kaggle_dependency_download.sh
+++ icevision_kaggle_dependency_download.sh
@@ -2,6 +2,7 @@
 # Unlike the install script, this always uses the version on Pypi for IceVision
 #@TODO: add link to example script to install from this data.
 
+echo "############# Step 1 of 6 ###################################"
 echo "Downloading icevision + dependencices for CUDA10"
 echo "- Downloading torch and its dependencies"
 pip download --dest "./pip/cuda10/" torch==1.10.0+cu102 torchvision==0.11.1+cu102 -f https://download.pytorch.org/whl/torch_stable.html -q
@@ -11,7 +12,8 @@ pip download --dest "./pip/cuda10/" mmcv-full==1.3.17 -f https://download.openmm
 
 
 
-echo "Downloading icevision + dependencices for CUDA111"
+echo "############# Step 2 of 6 ###################################"
+echo "Downloading icevision + dependencices for CUDA11"
 echo "- Downloading torch and its dependencies"
 pip download --dest "./pip/cuda11/" torch==1.10.0+cu111 torchvision==0.11.1+cu111 -f https://download.pytorch.org/whl/torch_stable.html -q
 
@@ -20,6 +22,7 @@ pip download --dest "./pip/cuda11/" mmcv-full==1.3.17 -f https://download.openmm
 
 
 
+echo "############# Step 3 of 6 ###################################"
 echo "Downloading icevision + dependencices for cpu"
 echo "- Downloading torch and its dependencies"
 pip download --dest "./pip/cpu/" torch=="1.10.0+cpu" torchvision=="0.11.1+cpu" -f https://download.pytorch.org/whl/torch_stable.html --q
@@ -28,10 +31,12 @@ echo "- Downloading mmcv"
 pip download --dest "./pip/cpu/" mmcv-full=="1.3.17" -f https://download.openmmlab.com/mmcv/dist/cpu/torch1.10.0/index.html -q
 
 
+echo "############# Step 4 of 6 ###################################"
 echo "- Downloading mmdet"
 pip download --dest "./pip/" mmdet==2.17.0 -q
 
 
+echo "############# Step 5 of 6 ###################################"
 echo "- Downloading icevision from PyPi"
 pip download --dest "./pip/icevision/" icevision[all] -q
 
@@ -42,8 +47,9 @@ echo "- Downloading yolov5-icevision"
 pip download --dest "./pip/icevision/" yolov5-icevision -q
 
 
+echo "############# Step 6 of 6 ###################################"
 echo "- Downloading torchtext"
 pip download --dest "/kaggle/working/pip" torchtext==0.11.0 -q
  
-
+echo "############# Done!       ###################################"
 echo "icevision download finished!"  
