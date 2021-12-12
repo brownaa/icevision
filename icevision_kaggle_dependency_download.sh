7cc1993d5f84539fb6082fe568a52f7f9b102661
Aaron Brown <brownaa@users.noreply.github.com>
2021-12-12 15:40:46 -0600

    fixed progress bar option flag

--- icevision_kaggle_dependency_download.sh
+++ icevision_kaggle_dependency_download.sh
@@ -4,46 +4,46 @@
 
 echo "Downloading icevision + dependencices for CUDA10"
 echo "- Downloading torch and its dependencies"
-pip download --dest "./pip/cuda10/" torch==1.10.0+cu102 torchvision==0.11.1+cu102 -f https://download.pytorch.org/whl/torch_stable.html --progress-bar no
+pip download --dest "./pip/cuda10/" torch==1.10.0+cu102 torchvision==0.11.1+cu102 -f https://download.pytorch.org/whl/torch_stable.html --progress-bar off
 
 echo "- Downloading mmcv"
-pip download --dest "./pip/cuda10/" mmcv-full==1.3.17 -f https://download.openmmlab.com/mmcv/dist/cu102/torch1.10.0/index.html --progress-bar no
+pip download --dest "./pip/cuda10/" mmcv-full==1.3.17 -f https://download.openmmlab.com/mmcv/dist/cu102/torch1.10.0/index.html --progress-bar off
 
 
 
 echo "Downloading icevision + dependencices for CUDA111"
 echo "- Downloading torch and its dependencies"
-pip download --dest "./pip/cuda11/" torch==1.10.0+cu111 torchvision==0.11.1+cu111 -f https://download.pytorch.org/whl/torch_stable.html --progress-bar no
+pip download --dest "./pip/cuda11/" torch==1.10.0+cu111 torchvision==0.11.1+cu111 -f https://download.pytorch.org/whl/torch_stable.html --progress-bar off
 
 echo "- Downloading mmcv"
-pip download --dest "./pip/cuda11/" mmcv-full==1.3.17 -f https://download.openmmlab.com/mmcv/dist/cu111/torch1.10.0/index.html --progress-bar no
+pip download --dest "./pip/cuda11/" mmcv-full==1.3.17 -f https://download.openmmlab.com/mmcv/dist/cu111/torch1.10.0/index.html --progress-bar off
 
 
 
 echo "Downloading icevision + dependencices for cpu"
 echo "- Downloading torch and its dependencies"
-pip download --dest "./pip/cpu/" torch=="1.10.0+cpu" torchvision=="0.11.1+cpu" -f https://download.pytorch.org/whl/torch_stable.html --progress-bar no
+pip download --dest "./pip/cpu/" torch=="1.10.0+cpu" torchvision=="0.11.1+cpu" -f https://download.pytorch.org/whl/torch_stable.html --progress-bar off
 
 echo "- Downloading mmcv"
-pip download --dest "./pip/cpu/" mmcv-full=="1.3.17" -f https://download.openmmlab.com/mmcv/dist/cpu/torch1.10.0/index.html --progress-bar no 
+pip download --dest "./pip/cpu/" mmcv-full=="1.3.17" -f https://download.openmmlab.com/mmcv/dist/cpu/torch1.10.0/index.html --progress-bar off 
 
 
 echo "- Downloading mmdet"
-pip download --dest "./pip/" mmdet==2.17.0 --progress-bar no
+pip download --dest "./pip/" mmdet==2.17.0 --progress-bar off
 
 
 echo "- Downloading icevision from PyPi"
-pip download --dest "./pip/icevision/" icevision[all] --progress-bar no
+pip download --dest "./pip/icevision/" icevision[all] --progress-bar off
 
 echo "- Downloading icedata from PyPi"      
-pip download --dest "./pip/icevision/" icedata --progress-bar no
+pip download --dest "./pip/icevision/" icedata --progress-bar off
 
 echo "- Downloading yolov5-icevision" 
-pip download --dest "./pip/icevision/" yolov5-icevision --progress-bar no
+pip download --dest "./pip/icevision/" yolov5-icevision --progress-bar off
 
 
-echo "- Downloading torchtext
-pip download --dest "/kaggle/working/pip" torchtext==0.11.0 --progress-bar no
+echo "- Downloading torchtext"
+pip download --dest "/kaggle/working/pip" torchtext==0.11.0 --progress-bar off
  
 
 echo "icevision download finished!"  
