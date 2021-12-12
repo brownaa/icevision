0bd53cb160432b28b5d47943e9cf6a36fe819376
Aaron Brown <brownaa@users.noreply.github.com>
2021-12-12 15:24:28 -0600

    creating initial IceVision downloader for Kaggle Dataset

---
+++ icevision_kaggle_dependency_download.sh
@@ -0,0 +1,49 @@
+# Downloads all dependencies for Cuda 10, 11 and for CPUs
+# Unlike the install script, this always uses the version on Pypi for IceVision
+#@TODO: add link to example script to install from this data.
+
+echo "Downloading icevision + dependencices for CUDA10"
+echo "- Downloading torch and its dependencies"
+pip download --dest "./pip/cuda10/" torch==1.10.0+cu102 torchvision==0.11.1+cu102 -f https://download.pytorch.org/whl/torch_stable.html --progress-bar pretty
+
+echo "- Downloading mmcv"
+pip download --dest "./pip/cuda10/" mmcv-full==1.3.17 -f https://download.openmmlab.com/mmcv/dist/cu102/torch1.10.0/index.html --progress-bar pretty
+
+
+
+echo "Downloading icevision + dependencices for CUDA111"
+echo "- Downloading torch and its dependencies"
+pip download --dest "./pip/cuda11/" torch==1.10.0+cu111 torchvision==0.11.1+cu111 -f https://download.pytorch.org/whl/torch_stable.html --progress-bar pretty
+
+echo "- Downloading mmcv"
+pip download --dest "./pip/cuda11/" mmcv-full==1.3.17 -f https://download.openmmlab.com/mmcv/dist/cu111/torch1.10.0/index.html --progress-bar pretty
+
+
+
+echo "Downloading icevision + dependencices for cpu"
+echo "- Downloading torch and its dependencies"
+pip download --dest "./pip/cpu/" torch=="1.10.0+cpu" torchvision=="0.11.1+cpu" -f https://download.pytorch.org/whl/torch_stable.html --progress-bar pretty
+
+echo "- Downloading mmcv"
+pip download --dest "./pip/cpu/" mmcv-full=="1.3.17" -f https://download.openmmlab.com/mmcv/dist/cpu/torch1.10.0/index.html --progress-bar pretty 
+
+
+echo "- Downloading mmdet"
+pip download --dest "./pip/" mmdet==2.17.0 --progress-bar pretty
+
+
+echo "- Downloading icevision from PyPi"
+pip download --dest "./pip/icevision/" icevision[all] --progress-bar pretty
+
+echo "- Downloading icedata from PyPi"      
+pip download --dest "./pip/icevision/" icedata --progress-bar pretty
+
+echo "- Downloading yolov5-icevision" 
+pip download --dest "./pip/icevision/" yolov5-icevision --progress-bar pretty
+
+
+echo "- Downloading torchtext
+pip download --dest "/kaggle/working/pip" torchtext==0.11.0 --progress-bar pretty
+ 
+
+echo "icevision download finished!"  
