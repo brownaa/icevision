d7a9f2a85cd4024ce81de508f6630b049c340cdb
Aaron Brown <brownaa@users.noreply.github.com>
2022-01-01 23:38:30 -0600

    trial 1 minimul install

--- icevision_kaggle_dependency_download.sh
+++ icevision_kaggle_dependency_download.sh
@@ -6,34 +6,20 @@ target="${1}"
 case ${target} in 
    cuda10)  
       echo "############# Step 1 of 4 ###################################"
-      echo "Downloading icevision + dependencices for CUDA10"
-      echo "- Downloading torch and its dependencies"
-      pip download --dest "." torch==1.10.0+cu102 torchvision==0.11.1+cu102 -f https://download.pytorch.org/whl/torch_stable.html
-
       echo "- Downloading mmcv"
-      pip download --dest "." mmcv-full==1.3.17 -f https://download.openmmlab.com/mmcv/dist/cu102/torch1.10.0/index.html
+      pip download --dest "." mmcv-full==1.3.17 -f https://download.openmmlab.com/mmcv/dist/cu102/torch1.9.0/index.html
     ;;
 
    cuda11)  
       echo "############# Step 1 of 4 ###################################"
-      echo "Downloading icevision + dependencices for CUDA11"
-      echo ""
-      echo "- Downloading torch and its dependencies"
-      pip download --dest "." torch==1.10.0+cu111 torchvision==0.11.1+cu111 -f https://download.pytorch.org/whl/torch_stable.html
-
-      echo ""
       echo "- Downloading mmcv"
-      pip download --dest "." mmcv-full==1.3.17 -f https://download.openmmlab.com/mmcv/dist/cu111/torch1.10.0/index.html
+      pip download --dest "." mmcv-full==1.3.17 -f https://download.openmmlab.com/mmcv/dist/cu111/torch1.9.0/index.html
     ;;
 
    cpu)
       echo "############# Step 1 of 4 ###################################"
-      echo "Downloading icevision + dependencices for cpu"
-      echo "- Downloading torch and its dependencies"
-      pip download --dest "." torch=="1.10.0+cpu" torchvision=="0.11.1+cpu" -f https://download.pytorch.org/whl/torch_stable.html
-
       echo "- Downloading mmcv"
-      pip download --dest "." mmcv-full=="1.3.17" -f https://download.openmmlab.com/mmcv/dist/cpu/torch1.10.0/index.html
+      pip download --dest "." mmcv-full=="1.3.17" -f https://download.openmmlab.com/mmcv/dist/cpu/torch1.9.0/index.html
    ;;
    
    *)  
@@ -48,17 +34,25 @@ pip download --dest "." mmdet==2.17.0
 
 echo "############# Step 3 of 4 ###################################"
 echo "- Downloading icevision from PyPi"
-pip download --dest "." icevision[all]
+pip download --dest "." icevision
+pip download --dest "." pycocotools<3
+pip download --dest "." loguru>=0.5.3
+pip download --dest "." pytorch-lightning>1.4.5
+pip download --dest "." effdet<0.3
+pip download --dest "." omegaconf<3
+#pip download --dest "." dataclasses==0.6
+pip download --dest "." resnest<0.0.7
+
 
 echo "- Downloading icedata from PyPi"      
-pip download --dest "." icedata
+# pip download --dest "." icedata
 
 echo "- Downloading yolov5-icevision" 
-pip download --dest "." yolov5-icevision
+# pip download --dest "." yolov5-icevision
 
 echo "############# Step 4 of 4 ###################################"
 echo "- Downloading torchtext"
-pip download --dest "." torchtext==0.11.0
+# pip download --dest "." torchtext==0.11.0
 
 echo "############# Done!       ###################################"
 echo "icevision download finished!"  
