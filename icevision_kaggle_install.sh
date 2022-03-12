c17cfe20d2cb62f3b68dcf56b37acd46cce21e21
Aaron Brown <brownaa@users.noreply.github.com>
2022-03-12 10:43:05 -0600

    removed -q pip install option

--- icevision_kaggle_install.sh
+++ icevision_kaggle_install.sh
@@ -3,20 +3,20 @@ echo ""
 
 echo "Installing antlr"
 cp -r ../input/icevision-essentials/icevision-repos/repos/antlr4-python3-runtime-4.8/antlr4-python3-runtime-4.8 /kaggle/working
-pip install ./antlr4-python3-runtime-4.8 -q
+pip install ./antlr4-python3-runtime-4.8
 rm -r ./antlr4-python3-runtime-4.8
 
 echo "Installing fire"
-pip install ../input/icevision-essentials/icevision-repos/repos/fire-0.4.0/fire-0.4.0.tar -q
+pip install ../input/icevision-essentials/icevision-repos/repos/fire-0.4.0/fire-0.4.0.tar
 
 echo "Installing fvcore"
 echo " - requires iopath"
-pip install ../input/icevision-essentials/icevision-repos/repos/iopath-0.1.9-py3-none-any.whl -q
+pip install ../input/icevision-essentials/icevision-repos/repos/iopath-0.1.9-py3-none-any.whl
 cp -r ../input/icevision-essentials/icevision-repos/repos/fvcore-0.1.5.post20220305/fvcore-0.1.5.post20220305 /kaggle/working
-pip install ./fvcore-0.1.5.post20220305 -q
+pip install ./fvcore-0.1.5.post20220305
 rm -r ./fvcore-0.1.5.post20220305
 
-pip install --no-index ../input/icevision-essentials/icevision-repos/repos/pycocotools-2.0.2/dist/pycocotools-2.0.2.tar -q
+pip install --no-index ../input/icevision-essentials/icevision-repos/repos/pycocotools-2.0.2/dist/pycocotools-2.0.2.tar
 
 # requires pycocotools, 
-pip install --no-index icevision[all] -f ../input/icevision-essentials/icevision-repos/repos -q
+pip install --no-index icevision[all] -f ../input/icevision-essentials/icevision-repos/repos
