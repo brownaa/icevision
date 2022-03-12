4ebbfbc8d2970c6e9dc420ffccaa87d2cdbad53f
Aaron Brown <brownaa@users.noreply.github.com>
2022-03-12 07:30:52 -0600

    moving everything into directory for compression

--- make_kaggle_dataset.sh
+++ make_kaggle_dataset.sh
@@ -1,3 +1,13 @@
 # Downloads requirements for pip install icevision[all]
-pip download pycocotools==2.0.2
-pip download icevision[all]
+mkdir repos
+
+pip download -d repos/ pycocotools==2.0.2
+pip download -d repos/ icevision[all]
+
+# create tar.gz file
+tar -czvf icevision-repos.tar.gz repos
+ls -l icevision-repos.tar.gz
+
+# remove folder
+rm -rf repos
+echo "repos directory removed"
