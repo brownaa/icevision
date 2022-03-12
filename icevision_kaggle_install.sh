e15cc26078d965c21b03fd84b668e3044db26c56
Aaron Brown <brownaa@users.noreply.github.com>
2022-03-12 10:54:44 -0600

    added -q options for pip install and added install IceVision config files

--- icevision_kaggle_install.sh
+++ icevision_kaggle_install.sh
@@ -3,25 +3,33 @@ echo "~~ Installing IceVision requirements from source ~~"
 echo ""
 echo "### Installing antlr ###"
 cp -r ../input/icevision-essentials/icevision-repos/repos/antlr4-python3-runtime-4.8/antlr4-python3-runtime-4.8 /kaggle/working
-pip install ./antlr4-python3-runtime-4.8
+pip install ./antlr4-python3-runtime-4.8 -q
 rm -r ./antlr4-python3-runtime-4.8
 
 echo ""
 echo "### Installing fire ###"
-pip install ../input/icevision-essentials/icevision-repos/repos/fire-0.4.0/fire-0.4.0.tar
+pip install ../input/icevision-essentials/icevision-repos/repos/fire-0.4.0/fire-0.4.0.tar -q
 
 echo""
 echo "### Installing fvcore ###"
 echo " - requires iopath"
-pip install ../input/icevision-essentials/icevision-repos/repos/iopath-0.1.9-py3-none-any.whl
+pip install ../input/icevision-essentials/icevision-repos/repos/iopath-0.1.9-py3-none-any.whl -q
 cp -r ../input/icevision-essentials/icevision-repos/repos/fvcore-0.1.5.post20220305/fvcore-0.1.5.post20220305 /kaggle/working
-pip install ./fvcore-0.1.5.post20220305
+pip install ./fvcore-0.1.5.post20220305 -q
 rm -r ./fvcore-0.1.5.post20220305
 
 echo ""
 echo "### Installing pycocotools ###"
-pip install --no-index ../input/icevision-essentials/icevision-repos/repos/pycocotools-2.0.2/dist/pycocotools-2.0.2.tar
+pip install --no-index ../input/icevision-essentials/icevision-repos/repos/pycocotools-2.0.2/dist/pycocotools-2.0.2.tar -q
 
 echo ""
 echo "### Installing IceVision ###"
-pip install --no-index icevision[all] -f ../input/icevision-essentials/icevision-repos/repos
+pip install --no-index icevision[all] -f ../input/icevision-essentials/icevision-repos/repos -q
+
+echo ""
+echo"~~ Installing IceVision configuration files ~~"
+echo ""
+rsync -a ../input/icevision-essentials/root/.config/Ultralytics/ /root/.config/Ultralytics/
+rsync -a ../input/icevision-essentials/root/.icevision /root
+
+echo "~~ Done ~~
